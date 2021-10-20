1ef08a41ef5252ab8ae414240e5fb3a25bf4085a
Igor de Lima Nunes <igorlnunes@gmail.com>
2021-10-19 23:10:15 -0300

    Modificacoes Resposta.tsx

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -25,7 +25,17 @@ export default function Resposta(props: RespostaProps) {
                     </div>
                 </div>
                 <div className={styles.verso}>
-
+                    {resposta.certa ? (
+                        <div className={styles.certa}>
+                            <div>A resposta certa é...</div>
+                            <div className={styles.valor}>{resposta.valor}</div>
+                        </div>
+                    ) : (
+                        <div className={styles.errada}>
+                            <div>A resposta informada está errada...</div>
+                            <div className={styles.valor}>{resposta.valor}</div>
+                        </div>
+                        )}
                 </div>
 
             </div>
